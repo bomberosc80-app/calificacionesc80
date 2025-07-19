6a95a8832e1a853f2ff11ed3f0b3cae191bbae3c
bomberosc80-app <sergionovo88@gmail.com>
2025-07-18 23:32:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ function mostrarFormulario() {
     const div = document.createElement('div');
     div.innerHTML = `
       <h3>Bombero ${id}</h3>
-      <label>Calificación (Depto Personal):</label>
       <textarea id="nota-${id}" rows="2" placeholder="Ej: 10"></textarea>
       <hr>
     `;
