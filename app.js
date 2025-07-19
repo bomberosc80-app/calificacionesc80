1c012d46ff8571653535793205f42d5ff70cf814
bomberosc80-app <sergionovo88@gmail.com>
2025-07-18 23:51:59 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let jefeActivo = null;
 let personalActivo = [];
 
 // Cargar CSV desde GitHub y parsear
+
 async function cargarJefes() {
   try {
     const response = await fetch('https://bomberosc80-app.github.io/calificacionesc80/jefes.csv');
